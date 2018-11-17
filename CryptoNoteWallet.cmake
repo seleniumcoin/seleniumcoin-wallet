48320d53ceb5d46b74f10549be887300c200a91d
bestchancecoin <45109769+bestchancecoin@users.noreply.github.com>
2018-11-17 05:13:19 +0300

    Update CryptoNoteWallet.cmake

--- CryptoNoteWallet.cmake
+++ CryptoNoteWallet.cmake
@@ -1,4 +1,4 @@
 
-set(CN_PROJECT_NAME "")
-set(CN_CURRENCY_DISPLAY_NAME "")
-set(CN_CURRENCY_TICKER "")
+set(CN_PROJECT_NAME "bestchancecoin")
+set(CN_CURRENCY_DISPLAY_NAME "Bestchancecoin")
+set(CN_CURRENCY_TICKER "BCC")
